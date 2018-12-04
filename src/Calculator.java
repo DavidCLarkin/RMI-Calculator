0dfe5e03865fb44c7178fa714fd96fea924c60bf
David Larkin <davidlarkin96@gmail.com>
2018-12-04 23:35:22 +0000

    Initial Commit

---
+++ src/Calculator.java
@@ -0,0 +1,25 @@
+/*
+ * @interface Calculator
+ * 
+ * Calculator is the Interface, extending Remote, that provides the methds to be implemented
+ * in the CalculatorServer class
+ * 
+ * @author David Larkin - 20070186
+ */
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface Calculator extends Remote
+{
+	public double add(double a, double b) throws RemoteException; 
+	 
+	public double sub(double a, double b) throws RemoteException; 
+	 
+	public double mul(double a, double b) throws RemoteException; 
+	 
+	public double div(double a, double b) throws RemoteException; 
+	
+	public double power(double a, double b) throws RemoteException;
+	
+	public double mod(double a, double b) throws RemoteException;
+}
